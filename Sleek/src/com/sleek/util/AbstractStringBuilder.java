6e77fc16d3f1e4c9555443896e9bf0542352a597
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-01-10 22:39:58 +0530

    sleek: AbstractStringBuilder

--- Sleek/src/com/sleek/util/AbstractStringBuilder.java
+++ Sleek/src/com/sleek/util/AbstractStringBuilder.java
@@ -26,4 +26,51 @@ abstract class AbstractStringBuilder {
     AbstractStringBuilder() {
         value = new char[INITIAL_CAPACITY];
     }
+
+    AbstractStringBuilder(int capacity) {
+        if (capacity < 0) {
+            throw new NegativeArraySizeException();
+        }
+
+        value = new char[capacity];
+    }
+
+    AbstractStringBuilder(String string) {
+        count = string.length();
+        value = new char[count + INITIAL_CAPACITY];
+        string.getChars(0, count, value, 0);
+    }
+
+    private void enlargeBuffer(int min) {
+        int newSize = ((value.length >> 1) + value.length) + 2;
+        char[] newData = new char[min > newSize ? min : newSize];
+        System.arraycopy(value, 0, newData, 0, count);
+        value = newData;
+    }
+
+    final void appendNull() {
+        int newSize = count + 4;
+
+    }
+
+    final void append0(String string) {
+        if (string == null) {
+            appendNull();
+            return;
+        }
+
+        int adding = string.length();
+        int newSize = count + adding;
+
+        if (newSize > value.length) {
+            enlargeBuffer(newSize);
+        }
+
+        string.getChars(0, adding, value, count);
+        count = newSize;
+    }
+
+    public int capacity() {
+        return value.length;
+    }
 }
